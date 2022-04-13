33790b48efe428edc16175f2d5175bd6ebaa9ccc
jb py dev <89211942+jrb11@users.noreply.github.com>
2022-04-13 11:04:34 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,20 @@
+from multiprocessing import context
+import re
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    context= {
+        'var':'jay'
+    }
+    return render(request, 'index.html', context)
+    #return HttpResponse("This is home page")
+
+def about(request):
+    return HttpResponse("This is about page")
+
+def services(request):
+    return HttpResponse("This is services page")
+
+def contact(request):
+    return HttpResponse("This is contact page")   
